74e3818459ef0b6d292750592cb4ca586a4f5507
Asta <vine9151@gmail.com>
2023-02-28 14:05:28 +0900

    feat: add paste feature for normal mode
    230228_14_05

--- lua/nvim-ultivisual/utils/paste.lua
+++ lua/nvim-ultivisual/utils/paste.lua
@@ -56,12 +56,40 @@ end
 
 function M.do_normal_paste()
   local present_column = vim.fn.col('.')
+  local present_line = vim.fn.line('.')
+  local yanked = vim.fn.getreg('"'):split('\n')
+
+  local front, back
+  local lines_table = {}
+  local cursor_pos = { (present_line-1)+(#yanked) }
 
   if present_column == 1 then
-    vim.api.nvim_feedkeys(t('P'), 'n', true)
+    front = vim.fn.getline(present_line):sub(1, present_column-1)
+    back = vim.fn.getline(present_line):sub(present_column)
   else
-    vim.api.nvim_feedkeys(t('p'), 'n', true)
+    front = vim.fn.getline(present_line):sub(1, present_column)
+    back = vim.fn.getline(present_line):sub(present_column+1)
   end
+
+  if #yanked == 1 then
+    front = front .. yanked[1]
+    table.insert(cursor_pos, front:len())
+    table.insert(lines_table, front .. back)
+
+  else
+    front = front .. yanked[1]
+    back = yanked[#yanked] .. back
+    table.insert(cursor_pos, yanked[#yanked]:len())
+
+    lines_table = yanked
+    table.remove(lines_table, 1)
+    table.remove(lines_table, #yanked)
+    table.insert(lines_table, 1, front)
+    table.insert(lines_table, back)
+  end
+
+  vim.api.nvim_buf_set_lines(0, present_line-1, present_line, true, lines_table)
+  vim.api.nvim_call_function('cursor', cursor_pos)
 end
 
 function M.do_insert_paste()
